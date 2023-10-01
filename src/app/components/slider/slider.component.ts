7f58538e7e839c57ca79b92e4c0b37bdcc9a859b
Mokhtar Ehab <ehabmokhtar32@gmail.com>
2023-10-01 14:44:31 +0200

    using http client module for get data from apis

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+
+  movies:any;
+  constructor(private http:HttpClient){}
+ 
+  ngOnInit(): void {
+    this.getPopularMovies();
+  }
+
+  getPopularMovies(){
+    this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=cbd6740c5d5509528864baba95df9c18'
+    ).subscribe(data=>{
+      this.movies = data
+    }
+    )
+  }
 
 }
